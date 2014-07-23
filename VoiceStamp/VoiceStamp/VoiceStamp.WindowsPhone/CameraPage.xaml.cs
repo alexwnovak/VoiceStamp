0ff5a6d2c35232ab58a465f07b438c335a9993c4
Alex Novak <alexwnovak@gmail.com>
2014-07-23 17:04:03 -0400

    +CameraPage blank page

---
+++ VoiceStamp/VoiceStamp/VoiceStamp.WindowsPhone/CameraPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace VoiceStamp
+{
+   public sealed partial class CameraPage : Page
+   {
+      public CameraPage()
+      {
+         InitializeComponent();
+      }
+   }
+}
