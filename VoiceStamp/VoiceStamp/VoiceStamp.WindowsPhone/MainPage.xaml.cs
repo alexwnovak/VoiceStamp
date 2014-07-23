23f88ab4550b01e06ab96e3e1ba02c75ff344958
Alex Novak <alexwnovak@gmail.com>
2014-07-23 16:33:44 -0400

    +Blank universal app solution

---
+++ VoiceStamp/VoiceStamp/VoiceStamp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace VoiceStamp
+{
+   public sealed partial class MainPage : Page
+   {
+      public MainPage()
+      {
+         InitializeComponent();
+
+         NavigationCacheMode = NavigationCacheMode.Required;
+      }
+
+      protected override void OnNavigatedTo( NavigationEventArgs e )
+      {
+      }
+   }
+}
